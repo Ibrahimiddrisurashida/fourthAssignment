c28cebeec788d5df6ba4e868573e4c53750305c4
Ibrahimiddrisurashida <ibrahimiddrisurashida@gmail.com>
2021-03-28 17:41:31 +0000

    commit 4

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer className="bg-success text-center text-sm-start text-white fixed-bottom">
+        <div className="container">
+            <h5 className="navbar-text">copyright &copy; IBRAHIM IDDRISU RASHIDA</h5>
+            <a className="text-white ml-5" href="mailto:ibrahimiddrisurashida@gmail.com">ibrahimiddrisurashida@gmail.com</a>
+        </div>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Loading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Loading() {
+    return (
+        <div className="spinner-grow text-warning" style={{height: 50, width: 50}}>
+        </div>
+    )
+}
+
+export default Loading
